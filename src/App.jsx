1fced9c083464c5b0d57bf53d8db6bd3fe2b4007
Shivakarka <shivakarka@gmail.com>
2022-08-12 17:05:06 +0530

    created required component files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
+import Billing from "./components/Billing";
+import Business from "./components/Business";
+import CardDeal from "./components/CardDeal";
+import Clients from "./components/Clients";
+import CTA from "./components/CTA";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Stats from "./components/Stats";
+import Testimonials from "./components/Testimonials";
 import styles from "./style";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div cclassName={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>Navbar</div>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>Hero</div>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
     </div>
 
     <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        Stats Business Billing CardDeal Testimonials Clients CTA Footer
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
       </div>
     </div>
   </div>
